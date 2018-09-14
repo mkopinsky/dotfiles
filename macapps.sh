77e74a0e10f341ec5a725549f9ce3d13d5733484
Michael Kopinsky <mkopinsky@gmail.com>
2018-09-14 18:26:34 -0400

    Add script with some additional mac app installs

---
+++ macapps.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Install Divvy Windows Manager
+mas install 413857545
+echo "If this is the first install of Divvy, you must enable it in System Preferences -> Security and Privacy -> Accessibility"
+# This doesn't work unfortunately. Oh well.
+# sudo sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db "INSERT INTO access VALUES('kTCCServiceAccessibility','com.mizage.Divvy',0,1,1,NULL);"
+
+# TODO: track divvy settings in dotfiles
+
+
+# Slack
+mas install 803453959
+
+# Whatsapp - this doesn't work. No clue why.
+mas install 1147396723
+
+# Wunderlist
+mas install 410628904
+
+# TickTick
+mas install 966085870
+
+
+# Turn on the `locate` daemon
+sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist
